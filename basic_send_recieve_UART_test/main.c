fa980a09cdb60a3520812660ac471a17d4a8e092
BENXBEN <32802278+benx13@users.noreply.github.com>
2020-02-22 09:38:19 +0100

    Add files via upload

---
+++ basic_send_recieve_UART_test/main.c
@@ -0,0 +1,52 @@
+//******************************************************************************
+//   MSP430G2xx3 Demo - USCI_A0, 9600 UART Echo ISR, DCO SMCLK
+//
+//   Description: Echo a received character, RX ISR used. 
+//   USCI_A0 RX interrupt triggers TX Echo.
+//   Baud rate divider with 1MHz = 1MHz/9600 = ~104.2
+//   ACLK = n/a, MCLK = SMCLK = 1MHz
+//
+//                MSP430G2553
+//             -----------------
+//         /|\|              XIN|-
+//          | |                 |
+//          --|RST          XOUT|-
+//            |                 |
+//            |     P1.2/UCA0TXD|------------>
+//            |                 | 9600 - 8N1
+//            |     P1.1/UCA0RXD|<------------
+//
+//******************************************************************************
+#include<msp430.h>
+
+int main(void)
+{
+  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+  DCOCTL = 0;                               // Select lowest DCOx and MODx settings
+  BCSCTL1 = CALBC1_1MHZ;                    // Set DCO
+  DCOCTL = CALDCO_1MHZ;
+  P1SEL |= BIT1 + BIT2 ;                    // P1.1 = RXD, P1.2=TXD
+  P1SEL2 |= BIT1 + BIT2 ;                   // P1.1 = RXD, P1.2=TXD
+  UCA0CTL1 |= UCSSEL_2;                     // SMCLK
+  UCA0BR0 = 104;                            // 1MHz 9600
+  UCA0BR1 = 0;                              // 1MHz 9600
+  UCA0MCTL = UCBRS0;                        // Modulation UCBRSx = 1
+  UCA0CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
+  IE2 |= UCA0RXIE;                          // Enable USCI_A0 RX interrupt
+
+  __bis_SR_register(GIE);                   //interrupts enabled
+}
+
+//  Echo back RXed character, confirm TX buffer is ready first
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector=USCIAB0RX_VECTOR
+__interrupt void USCI0RX_ISR(void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(USCIAB0RX_VECTOR))) USCI0RX_ISR (void)
+#else
+#error Compiler not supported!
+#endif
+{
+  while (!(IFG2&UCA0TXIFG));                // USCI_A0 TX buffer ready?
+  UCA0TXBUF = UCA0RXBUF;  // TX -> RXed character
+}
