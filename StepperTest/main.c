b6798b9d6cb547649f3edfd6f8de360f89dbe311
BENXBEN <32802278+benx13@users.noreply.github.com>
2020-02-26 12:25:40 +0100

    Add files via upload

---
+++ StepperTest/main.c
@@ -0,0 +1,46 @@
+#include<msp430g2553.h> 
+
+/*
+ * main.c
+ */
+
+
+
+
+
+void rotateMotor(int steps, char direction); //steps: number of motor steps(impules), direction:CW/CCW
+
+
+
+
+
+int main(void) 
+	{
+	WDTCTL = WDTPW | WDTHOLD;                // Stop watchdog timer
+	
+	DCOCTL = 0;                              // Select lowest DCOx and MODx settings
+	BCSCTL1 = CALBC1_16MHZ;                  // Set DCO
+	DCOCTL = CALDCO_16MHZ;
+
+
+	P2DIR |= (0x01 + 0x02);
+	P2OUT &=~(0x01 + 0x02);
+
+	rotateMotor(1000, FORWARD);
+	__delay_cycles(1000000);
+	}
+
+
+void rotateMotor(int steps, char direction)
+	{
+	int i=0;
+	if(direction == 1)P2OUT |= 0x01;
+	else P2OUT &=~ 0x01;
+	for (i; i<steps; i++)                      // Send impulsions 
+		{
+		P2OUT |= 0x02;                         // P2.1 set to high
+		__delay_cycles(10000);
+		P2OUT &=~ 0x02;                        // P2.1 set to low 
+		__delay_cycles(10000);                
+		}
+	}
